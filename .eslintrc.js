ace0d69dbc6275e4de1a6b47953b4717f20f04a1
Ken Eucker <keneucker@gmail.com>
2021-12-07 17:50:55 -0800

    Fixes the build and resolves the random no-explicit-any on all .vue files.

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,8 +30,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-explicit-any': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-
+    '@typescript-eslint/no-explicit-any': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     // this rule, if on, would require explicit return type on the `render` function
     '@typescript-eslint/explicit-function-return-type': 'off',
   },
