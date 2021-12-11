2b1e5a9e662fc1df3b12b8053134c6ef78f3e839
fairdev610 <fairdev9610@outlook.com>
2021-12-11 08:57:52 +0300

    Large bicon

--- src/components/PlayerAvatar.vue
+++ src/components/PlayerAvatar.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="player-wrapper p-1 mw-min" role="button" @click="goPlayerPage">
+  <div :class="'player-wrapper avatar-' + size + ' p-1 mw-min'" role="button" @click="goPlayerPage">
     <span class="player-name p-1">{{ playerName }}</span>
-    <span v-if="tagCount" class="tag-count p-1">({{ tagCount }})</span>
+    <span v-if="tagCount" class="tag-count p-2">{{ tagCount }}</span>
     <img class="player-bicon" :src="playerAvatarUrl" :alt="playerName" />
     <!-- <span v-if="!!playerPos" class="p-1">Top{{ playerPos }}</span> -->
   </div>
@@ -13,6 +13,10 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'PlayerAvatar',
   props: {
+    size: {
+      type: String,
+      default: 'lg',
+    },
     playerPos: {
       type: Number,
       default: 0,
@@ -43,33 +47,65 @@ export default defineComponent({
 
   .player-name {
     position: absolute;
-    bottom: 1rem;
-    top: auto;
     left: 50%;
     -webkit-transform: translateX(-50%);
     transform: translateX(-50%);
     z-index: 99;
     padding: 0 0.5rem;
     text-shadow: 2px 2px #292828e6;
-
-    /* border-radius: 10px; */
   }
 
   .tag-count {
     position: absolute;
-    top: 1rem;
-    right: 1rem;
+    padding: 20px;
+    width: 7vh;
+    text-align: center;
+    clip-path: circle(40%);
     z-index: 99;
-    text-shadow: 2px 2px #292828e6;
   }
 }
+
+.avatar-lg {
+  .player-bicon {
+    border-radius: 5%;
+    max-width: 50vh;
+  }
+  .player-name {
+    bottom: 1rem;
+    top: auto;
+  }
+  .tag-count {
+    top: 1rem;
+    right: 1rem;
+  }
+}
+
+.avatar-md {
+  .player-bicon {
+    clip-path: circle(40%);
+    max-width: 50vh;
+  }
+  .player-name {
+    bottom: 30%;
+    top: auto;
+  }
+  .tag-count {
+    top: 45%;
+    right: 4rem;
+  }
+}
+
+.avatar-sm {
+  .player-bicon {
+    border-radius: 5%;
+    max-width: 50vh;
+  }
+}
+
 span {
   font-size: 4vh;
 }
-.player-bicon {
-  border-radius: 5%;
-  max-width: 50vh;
-}
+
 .mw-min {
   max-width: min-content;
 }
