27ac896f1feba79031ce86f881e7a1ec3c65646e
Ken Eucker <keneucker@gmail.com>
2021-12-10 18:23:38 -0800

    Adds loading animation to the PlayView.

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -20,26 +20,34 @@
         :mystery-image-url="getCurrentBikeTag.mysteryImageUrl"
         :player="getCurrentBikeTag.mysteryPlayer"
         mystery-description="CURRENT MYSTERY LOCATION TO FIND"
+        @load="tagLoaded"
       />
-      <bike-tag v-if="tagnumber !== 0" :tag="tag" />
+      <bike-tag v-if="tagnumber !== 0" :tag="tag" @load="tagLoaded" />
     </div>
   </div>
+  <loading v-if="tagIsLoading" v-model:active="tagIsLoading" :is-full-page="fullPage">
+    <img class="spinner" src="images/SpinningBikeV1.svg" />
+  </loading>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
 import { mapGetters } from 'vuex'
 import BikeTag from '@/components/BikeTag.vue'
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
 
 export default defineComponent({
   name: 'PlayView',
   components: {
     BikeTag,
+    Loading,
   },
   data() {
     console.log(this.$route.params)
     return {
       tagnumber: this.$route.params?.tagnumber?.length ? parseInt(this.$route.params.tagnumber) : 0,
+      tagIsLoading: false,
     }
   },
   computed: {
@@ -52,12 +60,19 @@ export default defineComponent({
       return undefined
     },
   },
+  created() {
+    this.tagIsLoading = true
+  },
   updated() {
+    // this.tagIsLoading = true
     this.$store.dispatch('setTags')
   },
   methods: {
     hint() {
       alert(this.getCurrentBikeTag.hint)
+    },
+    tagLoaded() {
+      this.tagIsLoading = false
     },
   },
 })
@@ -72,5 +87,19 @@ export default defineComponent({
   right: 20px;
   z-index: 99;
   font-size: 1.25em;
+}
+.spinner {
+  animation-name: spin;
+  animation-duration: 1000ms;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+}
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
 }
 </style>
