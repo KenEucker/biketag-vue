a25da907be885d44fc3f4e24ef33438c2e29ee91
Ken Eucker <keneucker@gmail.com>
2022-01-23 11:52:35 -0800

    Fixes issues with the authorize endpoints error flow.

--- functions/authorize.ts
+++ functions/authorize.ts
@@ -14,36 +14,34 @@ const authorizeHandler: Handler = async (event) => {
   let body = 'missing client key and token information'
   let statusCode = 401
 
-  if (
-    getBikeTagHash(clientKey) === clientToken &&
-    controlCheck === clientToken &&
-    clientKey?.length > 0 &&
-    clientToken?.length > 0 &&
-    accessToken?.length > 0
-  ) {
-    const biketagOpts = getBikeTagClientOpts({
-      ...event,
-      method: event.httpMethod,
-    } as unknown as request.Request)
-    const biketag = new BikeTagClient(biketagOpts)
-    const config = biketag.config()
-    const isValidAccessToken =
-      config.biketag.accessToken === accessToken ||
-      config.imgur?.refreshToken === accessToken ||
-      config.sanity?.token === accessToken ||
-      config.reddit?.refreshToken === accessToken ||
-      config.twitter?.bearer_token === accessToken
+  if (clientKey?.length > 0 && clientToken?.length > 0 && accessToken?.length > 0) {
+    if (getBikeTagHash(clientKey) === clientToken && controlCheck === clientToken) {
+      const biketagOpts = getBikeTagClientOpts({
+        ...event,
+        method: event.httpMethod,
+      } as unknown as request.Request)
+      const biketag = new BikeTagClient(biketagOpts)
+      const config = biketag.config()
+      const isValidAccessToken =
+        config.biketag.accessToken === accessToken ||
+        config.imgur?.refreshToken === accessToken ||
+        config.sanity?.token === accessToken ||
+        config.reddit?.refreshToken === accessToken ||
+        config.twitter?.bearer_token === accessToken
 
-    if (isValidAccessToken) {
-      statusCode = 200
-      switch (grantType) {
-        case 'refresh_token':
-          body = getBikeTagHash(`${clientKey}${clientToken}${accessToken}`)
-          break
-        default:
-          body = 'grant type not supported'
-          statusCode = 401
-          break
+      if (isValidAccessToken) {
+        statusCode = 200
+        switch (grantType) {
+          case 'refresh_token':
+            body = getBikeTagHash(`${clientKey}${clientToken}${accessToken}`)
+            break
+          default:
+            body = 'grant type not supported'
+            statusCode = 401
+            break
+        }
+      } else {
+        body = 'credentials do not pass control check'
       }
     } else {
       body = 'invalid access token'
