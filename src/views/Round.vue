fa3f64362fb49cfcea61b72aedbf2712e5684a77
ehoraizon <erich.info.work@gmail.com>
2023-02-13 23:45:52 -0500

    Fixes ExpandableImage.vue rendering in Round.vue .

--- src/views/Round.vue
+++ src/views/Round.vue
@@ -26,7 +26,7 @@
 </template>
 
 <script setup name="QueueBikeTagView">
-import { defineProps, ref, inject, computed, onMounted } from 'vue'
+import { defineProps, ref, computed, onMounted } from 'vue'
 import { useStore } from '@/store/index.ts'
 import { BiketagFormSteps } from '@/common/types'
 import { useTimer } from 'vue-timer-hook'
@@ -44,10 +44,6 @@ const props = defineProps({
   },
 })
 
-// data
-// const countDown = ref(10)
-// const queueError = ref(null)
-// const toast = inject('toast')
 const time = new Date()
 time.setSeconds(time.getSeconds() + 900) // 10 minutes timer
 const timer = ref(useTimer(time.getSeconds()))
@@ -55,99 +51,10 @@ const uploadInProgress = ref(false)
 const store = useStore()
 const { t } = useI18n()
 
-// computed
-// const getPlayerTag = computed(() => store.getPlayerTag)
-// const getGameName = computed(() => store.getGameName)
 const getFormStep = computed(() => store.getFormStep)
 const getCurrentBikeTag = computed(() => store.getCurrentBikeTag)
 const getQueuedTags = computed(() => store.getQueuedTags)
 
-// methods
-// function countDownTimer() {
-//   if (countDown.value > 0) {
-//     setTimeout(() => {
-//       countDown.value -= 1
-//       countDownTimer()
-//     }, 500)
-//   }
-// }
-// function isSubmittingData() {
-//   return (
-//     !isViewingQueue() &&
-//     !(
-//       getFormStep.value === BiketagFormSteps[BiketagFormSteps.roundJoined] ||
-//       getFormStep.value === BiketagFormSteps[BiketagFormSteps.roundPosted] ||
-//       getFormStep.value === BiketagFormSteps[BiketagFormSteps.shareBikeTagPost]
-//     )
-//   )
-// }
-// async function onQueueSubmit(newTagSubmission) {
-//   const { tag, formAction, formData, storeAction } = newTagSubmission
-//   if ('scrollRestoration' in history) {
-//     history.scrollRestoration = 'manual'
-//   }
-//   window.scrollTo(0, 0)
-
-//   toast.open({
-//     message: t('notifications.uploading'),
-//     type: 'info',
-//     position: 'top',
-//   })
-//   const errorAction = queueError.value.getAttribute('action')
-
-//   uploadInProgress.value = true
-//   const success = await store[storeAction](tag)
-//   uploadInProgress.value = false
-
-//   if (success === true) {
-//     /// Get a clean cache
-//     await store.setTags(true)
-//     /// Update the queue
-//     store.setQueuedTags(true)
-
-//     formData.set('game', getGameName.value)
-//     formData.set('tag', JSON.stringify(getPlayerTag.value))
-//     formData.set(
-//       'submission',
-//       `${getGameName.value}-${getPlayerTag.value.tagnumber}--${getPlayerTag.value.foundPlayer}`
-//     )
-
-//     if (tag.foundImage) {
-//       formData.set('foundImageUrl', getPlayerTag.value.foundImageUrl)
-//     } else if (tag.mysteryImage) {
-//       formData.set('mysteryImageUrl', getPlayerTag.value.mysteryImageUrl)
-//     }
-//     return sendNetlifyForm(
-//       formAction,
-//       new URLSearchParams(formData).toString(),
-//       () => {
-//         toast.open({
-//           message: `${storeAction} ${t('notifications.success')}`,
-//           type: 'success',
-//           position: 'top',
-//         })
-//       },
-//       (m) => {
-//         toast.open({
-//           message: `${t('notifications.error')} ${m}`,
-//           type: 'error',
-//           timeout: false,
-//           position: 'bottom',
-//         })
-//         return sendNetlifyError(m, undefined, errorAction)
-//       }
-//     )
-//   } else {
-//     const message = `${t('notifications.error')}: ${success}`
-//     toast.open({
-//       message,
-//       type: 'error',
-//       timeout: false,
-//       position: 'bottom',
-//     })
-//     return sendNetlifyError(message, undefined, errorAction)
-//   }
-// }
 function isViewingQueue() {
   return getFormStep.value === BiketagFormSteps[BiketagFormSteps.viewRound]
 }
@@ -156,7 +63,6 @@ function isViewingQueue() {
 const created = async () => {
   await store.setCurrentBikeTag(true)
   await store.setQueuedTags(true)
-  // countDownTimer()
 }
 created()
 
