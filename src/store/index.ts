7b620a089ef8ea88c460d9b183852cc9f26cd70e
Ken Eucker <keneucker@gmail.com>
2025-07-19 13:05:01 -0700

    Woop.s.

--- src/store/index.ts
+++ src/store/index.ts
@@ -163,7 +163,11 @@ export const useBikeTagStore = defineStore(BikeTagDefaults.store, {
     async setProfile(profile: any, token?: string) {
       /// Call to backend api GET on /profile with authorization header
       if (profile) {
-        this.auth0Token = this.auth0Token ?? token ?? profile.token ?? ''
+        this.auth0Token = token?.length
+          ? token
+          : this.auth0Token?.length
+            ? this.auth0Token
+            : (token ?? profile.token ?? '')
         profile.token = undefined
 
         const response = await client
